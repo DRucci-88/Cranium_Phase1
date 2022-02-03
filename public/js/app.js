839c0e317a8dfde80715d0308cfbd5c0d441fbde
irfanfiriezky <irfanfiriezkybahri@gmail.com>
2022-02-03 13:45:43 +0700

    Pemberian Container pada bagian Swiper Guna Melakukan Integrasi Backend

--- public/js/app.js
+++ public/js/app.js
@@ -10590,56 +10590,13 @@ var SwiperLearning = function SwiperLearning() {
     },
     modules: [swiper_1.FreeMode, swiper_1.Navigation, swiper_1.Thumbs],
     className: "mySwiper2"
-  }, react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-1.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-2.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-3.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-4.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-5.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-6.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-7.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-8.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-9.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-10.jpg"
-  }))), react_1["default"].createElement(react_2.Swiper, {
-    onSwiper: setThumbsSwiper,
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-    modules: [swiper_1.FreeMode, swiper_1.Navigation, swiper_1.Thumbs],
-    className: "mySwiper"
-  }, react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-1.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-2.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-3.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-4.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-5.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-6.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-7.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-8.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-9.jpg"
-  })), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("img", {
-    src: "https://swiperjs.com/demos/images/nature-10.jpg"
-  }))));
+  }, react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("div", {
+    className: "flex-container"
+  }, react_1["default"].createElement("div", null, "1"), react_1["default"].createElement("div", null, "2"), react_1["default"].createElement("div", null, "3"))), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("div", {
+    className: "flex-container"
+  }, react_1["default"].createElement("div", null, "1"), react_1["default"].createElement("div", null, "2"))), react_1["default"].createElement(react_2.SwiperSlide, null, react_1["default"].createElement("div", {
+    className: "flex-container"
+  }, react_1["default"].createElement("div", null, "1")))));
 };
 
 exports["default"] = SwiperLearning;
@@ -11185,7 +11142,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "#app { height: 100% }\r\nhtml,\r\nbody {\r\n  position: relative;\r\n  height: 100%;\r\n}\r\n\r\nbody {\r\n  background: #eee;\r\n  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;\r\n  font-size: 14px;\r\n  color: #000;\r\n  margin: 0;\r\n  padding: 0;\r\n}\r\n\r\n.swiper {\r\n  width: 100%;\r\n  height: 100%;\r\n}\r\n\r\n.swiper-slide {\r\n  text-align: center;\r\n  font-size: 18px;\r\n  background: #fff;\r\n\r\n  /* Center slide text vertically */\r\n  display: flex;\r\n  justify-content: center;\r\n  align-items: center;\r\n}\r\n\r\n.swiper-slide img {\r\n  display: block;\r\n  width: 100%;\r\n  height: 100%;\r\n  -o-object-fit: cover;\r\n     object-fit: cover;\r\n}\r\n\r\nbody {\r\n  background: #000;\r\n  color: #000;\r\n}\r\n\r\n.swiper {\r\n  width: 100%;\r\n  height: 300px;\r\n  margin-left: auto;\r\n  margin-right: auto;\r\n}\r\n\r\n.swiper-slide {\r\n  background-size: cover;\r\n  background-position: center;\r\n}\r\n\r\n.mySwiper2 {\r\n  height: 80%;\r\n  width: 100%;\r\n}\r\n\r\n.mySwiper {\r\n  height: 20%;\r\n  box-sizing: border-box;\r\n  padding: 10px 0;\r\n}\r\n\r\n.mySwiper .swiper-slide {\r\n  width: 25%;\r\n  height: 100%;\r\n  opacity: 0.4;\r\n}\r\n\r\n.mySwiper .swiper-slide-thumb-active {\r\n  opacity: 1;\r\n}\r\n\r\n.swiper-slide img {\r\n  display: block;\r\n  width: 100%;\r\n  height: 100%;\r\n  -o-object-fit: cover;\r\n     object-fit: cover;\r\n}\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "#app { height: 100% }\nhtml,\nbody {\n  position: relative;\n  height: 100%;\n}\n\nbody {\n  background: #eee;\n  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;\n  font-size: 14px;\n  color: #000;\n  margin: 0;\n  padding: 0;\n}\n\n.swiper {\n  width: 100%;\n  height: 100%;\n}\n\n.swiper-slide {\n  text-align: center;\n  font-size: 18px;\n  background: #fff;\n\n  /* Center slide text vertically */\n  display: flex;\n  justify-content: center;\n  align-items: center;\n}\n\n.swiper-slide img {\n  display: block;\n  width: 100%;\n  height: 100%;\n  -o-object-fit: cover;\n     object-fit: cover;\n}\n\nbody {\n  background: #000;\n  color: #000;\n}\n\n.swiper {\n  width: 100%;\n  height: 300px;\n  margin-left: auto;\n  margin-right: auto;\n}\n\n.swiper-slide {\n  background-size: cover;\n  background-position: center;\n}\n\n.mySwiper2 {\n  height: 100%;\n  width: 100%;\n}\n\n.mySwiper {\n  height: 20%;\n  box-sizing: border-box;\n  padding: 10px 0;\n}\n\n.mySwiper .swiper-slide {\n  width: 25%;\n  height: 100%;\n  opacity: 0.4;\n}\n\n.mySwiper .swiper-slide-thumb-active {\n  opacity: 1;\n}\n\n.swiper-slide img {\n  display: block;\n  width: 100%;\n  height: 100%;\n  -o-object-fit: cover;\n     object-fit: cover;\n}\n\n.flex-container {\n  display: flex;\n  flex-wrap: wrap;\n  background-color: DodgerBlue;\n  justify-content: center;\n}\n\n.flex-container>div {\n  background-color: #f1f1f1;\n  width: 200px;\n  margin: 100px;\n  text-align: center;\n  line-height: 200px;\n  font-size: 20px;\n}\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
